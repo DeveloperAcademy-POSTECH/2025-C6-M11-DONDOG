6146ce4e234f25425b39dd2362d26ca0dd4b0d00
nuyhhyun <sarahs19991115@gmail.com>
2025-10-05 00:46:16 +0900

    feat: 누끼 따기 기능 구현

--- DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/PostView.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/PostView.swift
@@ -12,9 +12,15 @@ struct PostView: View {
     
     var body: some View {
         // 이미지 로드 실패 시 어떻게 할 건 지 논의 후 예외 처리 예정
-        Image(uiImage: viewModel.image ?? UIImage())
-            .resizable()
-            .scaledToFit()
+        Group {
+                    if let image = viewModel.image {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Color.gray // 이미지가 없을 때 기본 배경
+                    }
+                }
             .onAppear {
                 viewModel.makeSticker()
             }

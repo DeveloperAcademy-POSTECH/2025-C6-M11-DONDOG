822445fd29a1b36a7948fa32899cee09130d62c3
nuyhhyun <sarahs19991115@gmail.com>
2025-10-10 10:47:17 +0900

    feat: caption ui 구현

--- DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/PostContentView.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/PostContentView.swift
@@ -13,11 +13,26 @@ struct PostContentView: View {
     var body: some View {
         ScrollView() {
             VStack {
-                Image(uiImage: viewModel.frontImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 465)
-                    .padding(.top, 23)
+                ZStack(alignment: .bottom) {
+                    Image(uiImage: viewModel.frontImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 465)
+                        .padding(.top, 23)
+                    HStack(spacing: 5) {
+                        let caption = viewModel.caption ?? ""
+                        ForEach(Array(caption.enumerated()), id: \.offset) { idx, char in
+                            Text(String(char))
+                                .font(.system(size: 20, weight: .regular))
+                                .padding(.vertical, 5)
+                                .padding(.horizontal, 8)
+                                .background(Color.gray)
+                                .cornerRadius(4)
+                        }
+
+                    }
+                    .padding(.bottom, 38)
+                }
                 Text("test")
             }
         }

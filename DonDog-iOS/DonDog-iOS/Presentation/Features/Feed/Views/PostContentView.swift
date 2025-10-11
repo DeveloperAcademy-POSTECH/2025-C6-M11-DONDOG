b0885cd80244b47ea9ad27e7925115451771ec57
nuyhhyun <sarahs19991115@gmail.com>
2025-10-11 16:27:25 +0900

    feat: 댓글 삭제 UI 구현

--- DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/PostContentView.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/PostContentView.swift
@@ -11,58 +11,66 @@ struct PostContentView: View {
     @StateObject var viewModel: PostViewModel
     @State private var image: UIImage = UIImage()
     @State private var showingFront = true
-
+    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 23) {
-                ZStack(alignment: .bottom) {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 465)
-                        .onTapGesture {
-                            showingFront.toggle()
-                            image = showingFront ? (viewModel.frontImage) : (viewModel.backImage)
-                        }
-                        .onReceive(viewModel.$frontImage) { newFront in
-                            if showingFront { image = newFront }
-                        }
-                        .onReceive(viewModel.$backImage) { newBack in
-                            if !showingFront { image = newBack }
-                        }
-                    
-                    HStack(spacing: 5) {
-                        let caption = viewModel.caption ?? ""
-                        ForEach(Array(caption.enumerated()), id: \.offset) { idx, char in
-                            Text(String(char))
-                                .font(.system(size: 20, weight: .regular))
-                                .padding(.vertical, 5)
-                                .padding(.horizontal, 8)
-                                .background(Color.gray)
-                                .cornerRadius(4)
-                        }
+        VStack(alignment: .leading, spacing: 23) {
+            ZStack(alignment: .bottom) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 465)
+                    .onTapGesture {
+                        showingFront.toggle()
+                        image = showingFront ? (viewModel.frontImage) : (viewModel.backImage)
                     }
-                    .padding(.bottom, 38)
-                }
+                    .onReceive(viewModel.$frontImage) { newFront in
+                        if showingFront { image = newFront }
+                    }
+                    .onReceive(viewModel.$backImage) { newBack in
+                        if !showingFront { image = newBack }
+                    }
                 
+                HStack(spacing: 5) {
+                    let caption = viewModel.caption ?? ""
+                    ForEach(Array(caption.enumerated()), id: \.offset) { idx, char in
+                        Text(String(char))
+                            .font(.system(size: 20, weight: .regular))
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, 8)
+                            .background(Color.gray)
+                            .cornerRadius(4)
+                    }
+                }
+                .padding(.bottom, 38)
+            }
+            
+            List {
                 ForEach(viewModel.comments) { comment in
                     VStack(alignment: .leading, spacing: 7) {
                         HStack(spacing: 20) {
                             Text(comment.author)
                                 .font(.system(size: 18))
-                                .foregroundStyle(Color.gray)
+                                .foregroundStyle(.gray)
                             Text(DataUtils.formatDate(comment.timestamp, format: "HH:mm"))
                                 .font(.system(size: 18))
-                                .foregroundStyle(Color.gray)
+                                .foregroundStyle(.gray)
                         }
                         
                         Text(comment.text)
                             .font(.system(size: 18))
-                            .foregroundStyle(Color.gray)
+                            .foregroundStyle(.gray)
                     }
-                    .padding(.bottom, 26)
+                    .padding(.vertical, 5)
+                }
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        // 삭제 로직
+                        //                            let comment = viewModel.comments[index]
+                        //                            viewModel.deleteComment(comment)
+                    }
                 }
             }
+            .listStyle(.plain)
         }
     }
 }

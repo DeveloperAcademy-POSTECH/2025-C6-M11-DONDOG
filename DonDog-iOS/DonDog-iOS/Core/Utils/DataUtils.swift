cd42d0493f0d2145efcb83b7918acd6acdf84679
ohcuy <everywavyday@gmail.com>
2025-10-18 15:08:10 +0900

    chore: formatTimeAgo 함수 추가 및 레퍼런스 파일 정리

--- DonDog-iOS/DonDog-iOS/Core/Utils/DataUtils.swift
+++ DonDog-iOS/DonDog-iOS/Core/Utils/DataUtils.swift
@@ -19,6 +19,24 @@ struct DataUtils {
         return formatter.string(from: date)
     }
     
+    static func formatTimeAgo(from date: Date) -> String {
+                let secondsAgo = Int(Date().timeIntervalSince(date))
+                
+                switch secondsAgo {
+                case 0..<60:
+                    return "지금"
+                case 60..<3600:
+                    return "\(secondsAgo / 60)분 전"
+                case 3600..<86400:
+                    return "\(secondsAgo / 3600)시간 전"
+                default:
+                    let formatter = DateFormatter()
+                    formatter.locale = Locale(identifier: "ko_KR")
+                    formatter.dateFormat = "MM월 dd일 HH:mm"
+                    return formatter.string(from: date)
+                }
+            }
+    
     static func relativeTimeString(from date: Date) -> String {
         let now = Date()
         let diff = now.timeIntervalSince(date)

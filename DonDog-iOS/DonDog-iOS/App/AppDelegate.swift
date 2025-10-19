d61ce32d33abc6ce494adf181014e33e7dc578f0
JUDY LEE <ljh.social@ewhain.net>
2025-10-19 20:37:24 +0900

    chore: 익명 로그인 삭제

--- DonDog-iOS/DonDog-iOS/App/AppDelegate.swift
+++ DonDog-iOS/DonDog-iOS/App/AppDelegate.swift
@@ -72,7 +72,6 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     // APNs device token → Firebase Auth
     func application(_ application: UIApplication,
                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
         // Firebase Auth (전화번호 인증용)
         Auth.auth().setAPNSToken(deviceToken, type: .prod)
         
@@ -85,7 +84,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
             self.uploadTokenToServer(token)
         }
     }
-
+    
     func application(_ application: UIApplication,
                      didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("APNs 등록 실패: \(error.localizedDescription)")

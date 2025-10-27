f041409421ca327b1d6c509e1bbca772709c0c4d
Ito <ckdwo23@gmail.com>
2025-10-27 14:46:06 +0900

    refact: data 매핑용 모델 생성

--- DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Models/DisplayablePost.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Models/DisplayablePost.swift
@@ -8,34 +8,7 @@
 import FirebaseCore
 import UIKit
 
-struct PostData: Codable {
-    let postId: String
-    let uid: String
-    let frontImageURL: String
-    let backImageURL: String
-    let caption: String
-    let createdAt: Timestamp
-    let updatedAt: Timestamp
-    let stickerPostId: String
-    let stickerType: String?
-    
-    init(postId: String, uid: String, frontImageURL: String, backImageURL: String, caption: String = "", createdAt: Timestamp? = nil, stickerPostId: String, stickerType: String?) {
-        self.postId = postId
-        self.uid = uid
-        self.frontImageURL = frontImageURL
-        self.backImageURL = backImageURL
-        self.caption = caption
-        self.createdAt = createdAt ?? Timestamp()
-        self.updatedAt = Timestamp()
-        self.stickerPostId = stickerPostId
-        self.stickerType = stickerType
-    }
-}
 
-extension PostData {
-    var frontURL: URL? { URL(string: frontImageURL) }
-    var backURL: URL?  { URL(string: backImageURL) }
-}
 
 // MARK: - DisplayablePost
 /// UI에서 표시하기 위한 게시물 모델 (Firestore 데이터 + 다운로드된 이미지 + 닉네임)

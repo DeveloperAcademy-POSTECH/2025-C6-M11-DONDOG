34147a672cd233c62d18274c3595d40aeea05aab
_hyun_ <52152418+nuyhhyun@users.noreply.github.com>
2025-10-28 11:15:21 +0900

    refact: StickerType 공통 enum으로 분리

--- DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/SubViews/StickerSheetView.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Features/Feed/Views/SubViews/StickerSheetView.swift
@@ -8,44 +8,6 @@
 import SwiftUI
 import UIKit
 
-enum StickerEmotion: String, CaseIterable {
-    case love = "사랑해"
-    case cool = "멋지다"
-    case what = "뭐야?"
-    case angry = "화나"
-    case sad = "슬퍼"
-    
-    var strokeColor: Color {
-        switch self {
-        case .love:
-            return .ddFeelingPink
-        case .cool:
-            return .ddFeelingYellow
-        case .what:
-            return .ddFeelingGreen
-        case .angry:
-            return .ddFeelingOrange
-        case .sad:
-            return .ddFeelingBlue
-        }
-    }
-    
-    var stickerDecoString: String {
-        switch self {
-        case .love:
-            return "loveSticker"
-        case .cool:
-            return "coolSticker"
-        case .what:
-            return "whatSticker"
-        case .angry:
-            return "angrySticker"
-        case .sad:
-            return "sadSticker"
-        }
-    }
-}
-
 struct StickerSheetView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var selectedEmotion: String? = nil

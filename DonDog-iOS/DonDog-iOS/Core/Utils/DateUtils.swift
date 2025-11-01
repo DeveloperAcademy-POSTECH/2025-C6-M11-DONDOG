0783c70362ee428d9491c3d996bf83b77cc05b63
ohcuy <everywavyday@gmail.com>
2025-11-01 19:54:25 +0900

    feat: DateUtils 수정

--- DonDog-iOS/DonDog-iOS/Core/Utils/DateUtils.swift
+++ DonDog-iOS/DonDog-iOS/Core/Utils/DateUtils.swift
@@ -17,12 +17,34 @@ struct DateUtils {
         case dayKey = "yyyy-MM-dd"
     }
     
+    private static var calendar: Calendar = {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(identifier: "Asia/Seoul") ?? .current
+        return calendar
+    }()
+    
+    // Date 타입의 값을 특정 형식의 문자열로 변환
     static func string(from date: Date, format: DateFormat) -> String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
-        formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        formatter.timeZone = self.calendar.timeZone
         formatter.dateFormat = format.rawValue
         return formatter.string(from: date)
+    }
+    
+    static func date(fromYear year: Int, month: Int, day: Int) -> Date? {
+        let components = DateComponents(year: year, month: month, day: day)
+        return calendar.date(from: components)
+    }
+    
+    // Date 값에서 년, 월, 일과 같은 구성 요소들을 DateComponents 타입으로 추출
+    static func components(from date: Date) -> DateComponents {
+        return calendar.dateComponents([.year, .month, .day], from: date)
+    }
+    
+    // 같은 날의 데이터를 묶기 위해 해당 날짜 반환 (자정)
+    static func startOfDay(for date: Date) -> Date {
+        return calendar.startOfDay(for: date)
     }
     
     static func relativeTimeString(from date: Date) -> String {

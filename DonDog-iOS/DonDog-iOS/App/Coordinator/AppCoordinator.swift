7e04437c2ada5092309983d802e3e66a8262a666
ohcuy <everywavyday@gmail.com>
2025-11-01 21:40:31 +0900

    feat: postDetail 변경사항 딥링크에 적용
    - 기타 리팩토링

--- DonDog-iOS/DonDog-iOS/App/Coordinator/AppCoordinator.swift
+++ DonDog-iOS/DonDog-iOS/App/Coordinator/AppCoordinator.swift
@@ -123,13 +123,22 @@ final class AppCoordinator: ObservableObject {
         }
         
         switch url.host {
-//        case "post":
-//            let roomId = components?.queryItems?.first(where: { $0.name == "roomId" })?.value
-//            let postId = components?.queryItems?.first(where: { $0.name == "postId" })?.value
-//            
-//            if let roomId = roomId, let postId = postId {
-//                push(.post(postId: postId, roomId: roomId))
-//            }
+        case "post":
+            let roomId = components?.queryItems?.first(where: { $0.name == "roomId" })?.value
+            let postId = components?.queryItems?.first(where: { $0.name == "postId" })?.value
+            
+            if let roomId = roomId, let postId = postId {
+                Task {
+                    do {
+                        let post: PostData = try await DataManager.shared.fetch(path: "Rooms/\(roomId)/posts/\(postId)")
+                        await MainActor.run {
+                            self.push(.postDetail(posts: [post], postType: .post))
+                        }
+                    } catch {
+                        print("게시물 이동 실패: \(error)")
+                    }
+                }
+            }
             
         default:
             // 처리할 수 없는 host일 경우 피드로 이동

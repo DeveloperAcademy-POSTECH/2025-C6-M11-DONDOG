28b7f62dd69e0cbcf9c14815634d32a9a448faf6
_hyun_ <52152418+nuyhhyun@users.noreply.github.com>
2025-11-02 16:29:44 +0900

    fix: archive에서 posts 정보 정상 전달

--- DonDog-iOS/DonDog-iOS/Presentation/Features/Archive/ViewModels/ArchiveViewModel.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Features/Archive/ViewModels/ArchiveViewModel.swift
@@ -51,7 +51,7 @@ final class ArchiveViewModel: ObservableObject {
 
         DispatchQueue.main.async {
             self.coordinator?.push(
-                .post(posts: posts, postType: .archive)
+                .post(posts: postsForDay, postType: .archive)
             )
         }
     }

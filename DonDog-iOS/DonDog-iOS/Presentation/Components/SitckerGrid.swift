90bb9c725601c87600b67724b27c836ad40ae8b8
_hyun_ <52152418+nuyhhyun@users.noreply.github.com>
2025-11-21 02:25:46 +0900

    design: 스티커 시트 stickerCell 프로그래스바 색상 변경

--- DonDog-iOS/DonDog-iOS/Presentation/Components/SitckerGrid.swift
+++ DonDog-iOS/DonDog-iOS/Presentation/Components/SitckerGrid.swift
@@ -107,7 +107,8 @@ struct StickerCellView: View {
         } else if isLoading {
             ZStack {
                 ProgressView()
-                    .tint(Color.ppPrime)
+                    .tint(showedAt == .sheet ? .ppGray300 : Color.ppPrime)
+                    .opacity(showedAt == .sheet ? 0.1 : 1.0)
                     .frame(width: 24, height: 24)
                     .padding(.horizontal, 48)
                     .padding(.top, 28)
